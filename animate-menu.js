7496b0fe56cddd41ab2f4ea5d8ecc49c5bf53e5a
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-05-11 17:27:03 +0200

    Fixing bugs connected with animating the menu

--- animate-menu.js
+++ animate-menu.js
@@ -1,42 +1,38 @@
+// ? Handlers.
 var socialMediaIcons = document.getElementsByClassName('fab');
 var menuButton = document.getElementById('toggle-menu');
 var icon = document.getElementById('menu-icon');
-var rotated = false;
+icon.style.transition = ".5s all ease";
 
+// ? Utility functions. 
+function setTransition() {
+    for(var i = 0; i < 3; i++) {
+        socialMediaIcons[i].style.transition = ".5s all ease";
+    }
+}
+setTransition();
 function hideIcons() {
     for(var i = 0; i < 3; i++) {
         socialMediaIcons[i].style.opacity = "0";
     }
 }
-
 function showIcons() {
     for(var i = 0; i < 3; i++) {
-        socialMediaIcons[i].style.transition = ".5s all ease";
         socialMediaIcons[i].style.opacity = "1";
     }
 }
 
-icon.style.transition = ".5s all ease";
+// ? Click listener.
+var rotated = false;
 menuButton.addEventListener('click', function() {
     if(!rotated) {
-        $(".fab").hover(function() {
-            $(this).css("cursor", "pointer");
-        }, function() {
-            $(this).css("cursor", "normal");
-        });
         icon.style.transform = "rotate(90deg)";
         rotated = true;
         showIcons();
-
-        // ! The animation isn't the way we want it to be.
     } else {
-        icon.style.transform = "rotate(-180deg)";
+        icon.style.transform = "rotate(0deg)";
         rotated = false;
         hideIcons();
-        $('.fab').unbind('mouseenter mouseleave');
-
-        // ! The above line doesn't disable the hover effect.
     }
 });
-
-// ! This file needs to be prettified.
+// $ Handling :hover effect with jQuery: https://forum.freecodecamp.org/t/disable-hover-feature-with-javascript-or-jquery/204695/10.
