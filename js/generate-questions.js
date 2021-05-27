c0aa816f9f017d61cfed017559b8a04de55ddb6d
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-05-27 18:07:31 +0200

    Initializing handlers

--- js/generate-questions.js
+++ js/generate-questions.js
@@ -1,18 +1,3 @@
-var questionHeader = new Array(11);
-var answerPlaceholder = new Array(11);
-
-for(var i = 1; i <= 10; i++) {
-    questionHeader[i] = document.getElementById('question-header' + i + '');
-}
-
-for(var i = 1; i <= 10; i++) {
-    answerPlaceholder[i] = document.getElementsByClassName('answer' + '');
-}
-
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-}
-
 class Question {
     constructor(heading, correctAnswer) {
         this.heading = heading;
@@ -20,17 +5,19 @@ class Question {
     }
 }
 
-var one = new Question('When did Romet celebrate its 50th anniversary?', 'In 1974');
-var two = new Question('How were the canadian Romet bikes called?', 'Tyler');
-var three = new Question("How was the girls Orion called?", 'Turing2');
-var four = new Question('How were the Romet tandems called?', 'Duet and Derby');
-var five = new Question('What does the abbreviation ZZR mean?', 'United Bike Workshop');
-var six = new Question('How many gears does the Dezamet 019 engine have?', 'Three');
-var seven = new Question('Which bike is Pasat an updated design of?', 'Wagant');
-var eight = new Question('What other batch is the limited Romet Polo M760 batch based on?', '700');
-var nine = new Question('What does the abbreviation 50-T-1 mean?', '50-Touristic-One');
-var ten = new Question('What does the abbreviaton GIGE-E mean?', 'Central Inspectorate of Energy Technology');
+// ? Objects of ten questions with the correct answers.
+var questionOne = new Question('When did Romet celebrate its 50th anniversary?', 'In 1974');
+var questionTwo = new Question('How were the canadian Romet bikes called?', 'Tyler');
+var questionThree = new Question("How was the girls Orion called?", 'Turing2');
+var questionFour = new Question('How were the Romet tandems called?', 'Duet and Derby');
+var questionFive = new Question('What does the abbreviation ZZR mean?', 'United Bike Workshop');
+var questionSix = new Question('How many gears does the Dezamet 019 engine have?', 'Three');
+var questionSeven = new Question('Which bike is Pasat an updated design of?', 'Wagant');
+var questionEight = new Question('What other batch is the limited Romet Polo M760 batch based on?', '700');
+var questionNine = new Question('What does the abbreviation 50-T-1 mean?', '50-Touristic-One');
+var questionTen = new Question('What does the abbreviaton GIGE-E mean?', 'Central Inspectorate of Energy Technology');
 
+// ? Arrays of answers to the ten questions:
 var answersOne = ['In 1970', 'In 1987', 'In 1974', 'This never happened'];
 var answersTwo = ['Unidal', 'Super Cycle', 'Tyler', 'Universal'];
 var answersThree = ['Just Orion', 'Kalina', 'Sava', 'Turing2'];
@@ -42,13 +29,47 @@ var answersEight = ['700', 'Ogar', '50-T-1', 'Pony'];
 var answersNine = ['50-Touristic-One', 'It is an ordinary factory indication', 'It does not mean anything important', 'It indicates the wheel size and engine type'];
 var answersTen = ['It is a quality standard', 'It does not mean anything important', 'Central Inspectorate of Human Factors and Ergonomics', 'Central Inspectorate of Energy Technology'];
 
-// ? More code...
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
 
-questionHeader.innerHTML = one.heading;
-for(var i = 0; i < 4; i++) {
-    if(i < 3) {
-        answerPlaceholder[i].innerHTML = answers[i] + ',';
+shuffle(answersOne);
+shuffle(answersTwo);
+shuffle(answersThree);
+shuffle(answersFour);
+shuffle(answersFive);
+shuffle(answersSix);
+shuffle(answersSeven);
+shuffle(answersEight);
+shuffle(answersNine);
+shuffle(answersTen);
+
+var headerOne = document.getElementById('question-header-1').innerHTML = questionOne.heading;
+var headerTwo = document.getElementById('question-header-2').innerHTML = questionTwo.heading;
+var headerThree = document.getElementById('question-header-3').innerHTML = questionThree.heading;
+var headerFour = document.getElementById('question-header-4').innerHTML = questionFour.heading;
+var headerFive = document.getElementById('question-header-5').innerHTML = questionFive.heading;
+var headerSix = document.getElementById('question-header-6').innerHTML = questionSix.heading;
+var headerSeven = document.getElementById('question-header-7').innerHTML = questionSeven.heading;
+var headerEight = document.getElementById('question-header-8').innerHTML = questionEight.heading;
+var headerNine = document.getElementById('question-header-9').innerHTML = questionNine.heading;
+var headerTen = document.getElementById('question-header-10').innerHTML = questionTen.heading;
+
+var answerPlaceholderOne = document.getElementsByClassName('answer-1');
+var answerPlaceholderTwo = document.getElementsByClassName('answer-2');
+var answerPlaceholderThree = document.getElementsByClassName('answer-3');
+var answerPlaceholderFour = document.getElementsByClassName('answer-4');
+var answerPlaceholderFive = document.getElementsByClassName('answer-5');
+var answerPlaceholderSix = document.getElementsByClassName('answer-6');
+var answerPlaceholderSeven = document.getElementsByClassName('answer-7');
+var answerPlaceholderEight = document.getElementsByClassName('answer-8');
+var answerPlaceholderNine = document.getElementsByClassName('answer-9');
+var answerPlaceholderTen = document.getElementsByClassName('answer-10');
+
+for (var i = 0; i < 4; i++) {
+    if (i < 3) {
+        answerPlaceholderOne[i].innerHTML = answersOne[i] + ',';
     } else {
-        answerPlaceholder[i].innerHTML = answers[i] + '.';
+        answerPlaceholderOne[i].innerHTML = answersOne[i] + '.';
     }
 }
